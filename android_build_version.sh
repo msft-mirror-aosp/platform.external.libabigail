6bca1bdb794a3b121d6bbb66da8f97ddcbcb99f5
Matthias Maennich <maennich@google.com>
2021-04-03 20:48:32 +0100

    version string: use pure git commit only
    `git describe` nowadays picks up some unrelated platform release tags
    and makes the version string very messy. Revert to just pure git
    commits. That should be good enough for now. We can consider using
    something like BUILD_NUMBER later, but this can't be passed via
    environment as it would break the hermeticity of the soong build.
    
    Signed-off-by: Matthias Maennich <maennich@google.com>
    Change-Id: I84ddaa27947dd02b8273c74ad16d524e7b10df10

--- android_build_version.sh
+++ android_build_version.sh
@@ -6,6 +6,6 @@ set -e
 dir="$1"
 out="$2"
 
-version=$(git -C "$dir" describe --abbrev=8 --always)-soong
+version=$(git -C "$dir" rev-parse --short=8 HEAD)-soong
 
 echo "#define ANDROID_BUILD_VERSION \"${version}\"" > "$out"
