fd878f215c23b62e908f23d98103ab68d177fc56
Dodji Seketeli <dodji@redhat.com>
2024-03-28 17:33:27 +0100

    ir: Fix Emacs C++ mode header
    	* src/abg-ir.cc: Fix the Emacs C++ mode header.
    
    Signed-off-by: Dodji Seketeli <dodji@redhat.com>

--- src/abg-ir.cc
+++ src/abg-ir.cc
@@ -1,6 +1,7 @@
-// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception -*- mode:
 // C++ -*-
-//
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+// -*- Mode: C++ -*-
+
 // Copyright (C) 2013-2023 Red Hat, Inc.
 //
 //Author: Dodji Seketeli
