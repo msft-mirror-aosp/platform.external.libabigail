msft-mirror-aosp/platform.external.libabigail

commit 666555665cc4fcfc8ae19661c489822e0df00ae3

ctf-reader: Fix re-initialization of the CTF reader
When analyzing a Linux Kernel tree made of vmlinux and loadable module
binaries, the same (CTF) binary reader is re-used to load every single
binaries in a loop.  That can be seen, for instance, by reading the
code of load_vmlinux_corpus in abg-tools-utils.cc.

As part of that process, abigail::ctf::reader::initialize is invoked
prior to using the reader to read type information from each binary.

But then, looking at things a bit closer, I realized that
ctf::reader::initialize is failing to reset the reader::ctfa data
member.  That leads to a memory leak that was making things grow
out of proportion.

Also, the resetting code fails to actually clear out the map of types
that are to be sorted and canonicalized.  That leads to unnecessarily
sorting huge amounts of types.

The patch address the two points above.

Apart from the obvious gain in code integrity, this patch
significantly reduces the time taken to analyze a Linux Kernel tree.
Please note that the subsequent patch coming after this one will
reduce that time even further.

	* src/abg-ctf-reader.cc (reader::reader): Initialize ctfa,
	ctf_sect, symtab_sect and strtab_sect data members.
	(reader::initialize): In the overload taking no argument, make
	sure to free the ctfa data member before setting it to nullptr.
	In the overload that takes arguments, make sure to invoke
	reader::initialize() to really free the data used.  Also, improve
	the comments.

Signed-off-by: Dodji Seketeli <dodji@redhat.com>

src/abg-ctf-reader.cc
@@ -241,7 +241,8 @@ public:
   reader(const string&		elf_path,
 	 const vector<char**>&	debug_info_root_paths,
 	 environment&		env)
-    : elf_based_reader(elf_path, debug_info_root_paths, env)
+    : elf_based_reader(elf_path, debug_info_root_paths, env),
+      ctfa(), ctf_sect(), symtab_sect(), strtab_sect()
   {
     initialize();
   }
@@ -251,23 +252,37 @@ public:
   /// This is useful to clear out the data used by the reader and get
   /// it ready to be used again.
   ///
-  /// Note that the reader eeps the same environment it has been
-  /// originally created with.
+  /// Note that the reader keeps (doesn't clear) the same environment
+  /// it has been originally created with.
   ///
   /// Please also note that the life time of this environment object
-  /// must be greater than the life time of the resulting @ref
-  /// reader the context uses resources that are allocated in
-  /// the environment.
+  /// must be greater than the life time of the resulting @ref reader
+  /// the context uses resources that are allocated in the
+  /// environment.
   void
   initialize()
   {
-    ctfa = nullptr;
+    if (ctfa)
+      {
+	ctf_close(ctfa);
+	ctfa = nullptr;
+      }
     types_map.clear();
     cur_tu_.reset();
     corpus_group().reset();
   }
 
   /// Initializer of the reader.
+  ///
+  /// This first makes sure the data used by the reader is cleared.
+  /// And then it initlizes it with the information passed in
+  /// argument.
+  ///
+  /// This is useful to clear out the data used by the reader and get
+  /// it ready to be used again.
+  ///
+  /// Note that the reader keeps the same environment it has been
+  /// originally created with.
   ///
   /// @param elf_path the new path to the new ELF file to use.
   ///
@@ -278,22 +293,13 @@ public:
   ///
   /// @param linux_kernel_mode currently not used.
   ///
-  /// This is useful to clear out the data used by the reader and get
-  /// it ready to be used again.
-  ///
-  /// Note that the reader eeps the same environment it has been
-  /// originally created with.
-  ///
-  /// Please also note that the life time of this environment object
-  /// must be greater than the life time of the resulting @ref
-  /// reader the context uses resources that are allocated in
-  /// the environment.
   void
-  initialize(const string& elf_path,
-             const vector<char**>& debug_info_root_paths,
-             bool load_all_types = false,
-             bool linux_kernel_mode = false)
+  initialize(const string&		elf_path,
+             const vector<char**>&	debug_info_root_paths,
+             bool			load_all_types = false,
+             bool			linux_kernel_mode = false)
   {
+    initialize();
     load_all_types = load_all_types;
     linux_kernel_mode = linux_kernel_mode;
     elf_based_reader::initialize(elf_path, debug_info_root_paths);

commit 57a7eb601131d8d4618fbe03b2fd794703619f3f

ctf-reader: During re-initialization, only clear canonicalize-able types
The patch
https://sourceware.org/git/?p=libabigail.git;a=commit;h=666555665cc4fcfc8ae19661c489822e0df00ae3
introduced a mistake: It frees the CTF archive object during the
re-initialization of the reader between each binary (kernel or
module).  The archive contains CTF type information for a kernel and
all its modules.  So it should be kept around until all those binaries
are analyzed.  Instead, that patch frees the CTF archive object after
handling each binary.  Oops.

This patch fixes that problem by free-ing the CTF archive only when
the reader itself is freed, presumably, after analyzing all binaries.

Similarly, the type map maintained by the reader contains types for
the kernel and all its modules.  So it should not be freed at
re-initialization time.  Rather, what should be freed is only the
types that are to be canonicalized at the end of processing of a given
binary.  So the patch makes the reader maintain types to be
canonicalized in a vector and clears that vector at each
re-initialization.

At re-initialization time, the patch also avoids resetting the corpus
group that the current reader feeds.

	* src/abg-ctf-reader.cc (reader::types_to_canonicalize): Add data
	member.
	(reader::add_type): Add the new type to the vector of types to be
	canonicalized.  Update comment.
	(reader::canonicalize_all_types): Now that the reader maintains
	the vector of types to be canonicalized, just pass that vector to
	canonicalize_types.  reader::types_map contains all the types that
	have been created in all the binaries processed by this reader so
	far.  Many of these types have already been canonicalized at the
	end of the analysis of binaries that have already been processed.
	Only the types created during the processing of the current binary
	have not yet be canonicalized and reader::types_to_canonicalize is
	where they are maintained.  So reader::types_map should be left

src/abg-ctf-reader.cc
@@ -145,6 +145,7 @@ class reader : public elf_based_reader
   /// A map associating CTF type ids with libabigail IR types.  This
   /// is used to reuse already generated types.
   string_type_base_sptr_map_type types_map;
+  vector<type_base_sptr> types_to_canonicalize;
 
   /// A set associating unknown CTF type ids
   std::set<ctf_id_t> unknown_types_set;
@@ -167,6 +168,10 @@ public:
 
   /// Associate a given CTF type ID with a given libabigail IR type.
   ///
+  /// The IR type is a newly created type that needs to be
+  /// canonicalized at the end of the processing of the current
+  /// corpus.
+  ///
   /// @param dic the dictionnary the type belongs to.
   ///
   /// @param ctf_type the type ID.
@@ -176,7 +181,8 @@ public:
   add_type(ctf_dict_t *dic, ctf_id_t ctf_type, type_base_sptr type)
   {
     string key = dic_type_key(dic, ctf_type);
-    types_map.insert(std::make_pair(key, type));
+    if (types_map.insert(std::make_pair(key, type)).second)
+      types_to_canonicalize.push_back(type);
   }
 
   /// Insert a given CTF unknown type ID.
@@ -216,12 +222,8 @@ public:
   void
   canonicalize_all_types(void)
   {
-    vector<type_base_sptr> types;
-    for (const auto& entry : types_map)
-      types.push_back(entry.second);
-
     canonicalize_types
-      (types.begin(), types.end(),
+      (types_to_canonicalize.begin(), types_to_canonicalize.end(),
        [](vector<type_base_sptr>::iterator& i)
        {return *i;});
   }
@@ -262,14 +264,8 @@ public:
   void
   initialize()
   {
-    if (ctfa)
-      {
-	ctf_close(ctfa);
-	ctfa = nullptr;
-      }
-    types_map.clear();
+    types_to_canonicalize.clear();
     cur_tu_.reset();
-    corpus_group().reset();
   }
 
   /// Initializer of the reader.
@@ -697,7 +693,7 @@ public:
     if ((corp->get_origin() & corpus::LINUX_KERNEL_BINARY_ORIGIN)
 	&& corpus_group())
       {
-	if (ctfa == NULL)
+	if (ctfa == nullptr)
 	  {
 	    std::string ctfa_filename;
 	    if (find_ctfa_file(ctfa_filename))
@@ -709,8 +705,9 @@ public:
 	 and process the CTF archive in the read context, if any.
 	 Information about the types, variables, functions, etc contained
 	 in the archive are added to the given corpus.  */
-      ctfa = ctf_arc_bufopen(&ctf_sect, &symtab_sect,
-			     &strtab_sect, &errp);
+      if (ctfa == nullptr)
+	ctfa = ctf_arc_bufopen(&ctf_sect, &symtab_sect,
+			       &strtab_sect, &errp);
 
     if (do_log())
       {
@@ -749,6 +746,7 @@ public:
   ~reader()
   {
     ctf_close(ctfa);
+    ctfa = nullptr;
   }
 }; // end class reader.
 